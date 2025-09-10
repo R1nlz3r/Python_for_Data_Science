261561c1b397c660c7f4476765339a026362adae
R1nlz3r <mapandel@student.42.fr>
2025-09-10 17:01:16 +0200

    Adds Python_2_DataTable/ex00

---
+++ Python_2_DataTable/ex00/load_csv.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+
+def load(path: str) -> pd.DataFrame:
+    try:
+        if not isinstance(path, str):
+            raise TypeError("Path is of the wrong type")
+        csv = pd.read_csv(path)
+    except FileNotFoundError:
+        print("FileNotFoundError: The file does not exist")
+        return None
+    except PermissionError:
+        print("PermissionError: Unable to read the file")
+        return None
+    except pd.errors.EmptyDataError:
+        print("EmptyDataError: The file header/data is empty")
+        return None
+    except UnicodeDecodeError:
+        print("UnicodeDecodeError: The codec cannot decode data in the file")
+        return None
+    except Exception as e:
+        print(f"{type(e).__name__}: {e}")
+        return None
+
+    print(f"Loading dataset of dimensions {csv.shape}")
+    return csv
